a01cd5b36a8e8777e659db175d72bc4c62d7baef
newtonkiragu <karanunewton4@gmail.com>
2017-07-17 19:45:31 +0300

    initial commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+require('sinatra')
+require('sinatra/reloader')
+require("pg")
+
+# require("sinatra/activerecord")
+# require('./lib/task')
+# require('./lib/list')
+# also_reload('lib/**/*.rb')
+
+get("/") do
+  @lists = List.all()
+  erb(:index)
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch("/tasks/:id") do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
+  erb(:index)
+end
