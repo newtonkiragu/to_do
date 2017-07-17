6cf3bbfff978892442a76d3d021a8272556e7eeb
newtonkiragu <karanunewton4@gmail.com>
2017-07-17 20:21:34 +0300

    added a spec and a method to confirm that a description has more than 50 words

--- lib/task.rb
+++ lib/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
-  validates(:description, :presence => true)
+  validates(:description, {:presence => true, :length => { :maximum => 50 }})
   belongs_to(:list)
   scope(:not_done, -> do
     where({:done => false})
